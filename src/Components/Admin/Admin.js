fb06e0e743ede7f1da22241e8a61cc6974493b5b
mb16biswas <64213233+mb16biswas@users.noreply.github.com>
2021-02-15 16:44:01 +0530

    Add files via upload

---
+++ src/Components/Admin/Admin.js
@@ -0,0 +1,80 @@
+import React ,{Component} from 'react' 
+import {Redirect} from "react-router-dom"
+import Mainpage from "../Mainpage/Mainpage"
+import Navbar from "../Navbar/Navbar"
+import Board from "../Board/Board"
+
+
+class Admin extends Component{
+    constructor(){
+        super()
+        let a = ""
+        let data = sessionStorage.getItem("mydata")
+        data = JSON.parse(data)
+        console.log("admin" , data)
+        
+        if(data !== null){
+            a = "admin"
+        }
+
+
+
+        this.state = {
+            test : a 
+
+        }
+    }
+    logout = () =>{
+        sessionStorage.removeItem("mydata")
+        this.setState({test : "logout"})
+
+    }
+    score = () =>{
+        this.setState({test : "score"})
+    }
+    cur_page = () =>{
+        this.setState({test : "admin"})
+
+    }
+
+
+    render(){
+        if(this.state.test === "admin"){
+            return(
+                <div>
+                    <Navbar  logout = {this.logout}  score = {this.score}  cur_page =  {this.cur_page} />
+                    <Mainpage/>
+                    
+
+                </div>
+              
+            )
+
+        }
+        else if(this.state.test === "score"){
+            return(
+                <div>
+                    <Navbar logout = {this.logout}  score = {this.score}  cur_page =  {this.cur_page}/>
+                    <Board/>
+              
+
+                </div>
+
+            )
+            
+    
+           
+
+        }
+        else{
+            return(
+                <Redirect to = "/"/>
+
+            )
+          
+        }
+        
+    }
+} 
+
+export default Admin 
